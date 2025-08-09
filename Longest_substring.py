9ac7da65d395d7423cd4299d741fcd646eaa11b9
vaibhavkheriwal <vaibhavkheriwal1@gmail.com>
2025-08-10 02:16:43 +0530

    Delete Longest_substring.py

--- Longest_substring.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a Python function to find the longest substring of a given string that contains exactly k distinct characters. If there are multiple substrings with the same length, return the first one that appears. Assume the input string contains only lowercase alphabets. 
-
-input_string = "eceba"
-k = 2
-Expected output: "ece"
-
-For input_string = "eceba" and k = 2, the longest substring with exactly 2 distinct characters is "ece".
-"""
